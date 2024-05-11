ae7c6a4a85d6e227b40d18566203b0d391abbede
NikkosVerati <hainc.fufelscmerc@gmail.com>
2024-05-11 18:12:30 +0400

    API[admin]: edit user data

--- src/main.py
+++ src/main.py
@@ -10,7 +10,8 @@ import logging
 class User(BaseModel):
     username: str
     password: str
-    role: str | None
+    role: str | None = None
+    token: str | None = None
 
 
 logger = Logger()
@@ -77,8 +78,44 @@ async def add_account(user: User):
     return JSONResponse(content={"message": "The user has been successfully added"}, status_code=200)
 
 
-@app.get("/api/admin/all_users")
+@app.get("/api/admin/users/get")
 async def get_all_users():  # Проверка на токен в будущем
     with open("data/users_auth_data.yaml") as file:
         read_data = yaml.load(file, Loader=yaml.FullLoader)
         return read_data['users']
+
+
+@app.post("/api/admin/users/edit")
+async def edit_user(user: User):
+    with open("data/users_auth_data.yaml", "r+") as file:
+        read_data = yaml.load(file, Loader=yaml.FullLoader)
+
+        if user.role not in read_data['roles']:
+            return JSONResponse(content={"message": "Error in the selected role"}, status_code=400)
+        elif any(user.username in el.keys() and
+                 user.token != list(el.values())[0]['token'] for el in read_data['users']):
+            return JSONResponse(content={"message": "Login already exists"}, status_code=409)
+
+        users = read_data["users"]
+        updated_users = []
+        for i in users:
+            if user.token == list(i.values())[0]['token']:
+                new_user = {user.username: {
+                    "password": user.password,
+                    "role": user.role,
+                    "token": user.token
+                }}
+                updated_users.append(new_user)
+                continue
+            else:
+                updated_users.append(i)
+
+        read_data['users'] = updated_users
+
+        logging.info(f"Save updated user data to file")
+        file.seek(0)
+        yaml.dump(read_data, file)
+        file.truncate()
+        logging.info(f"Successful save user data to file")
+
+        return JSONResponse(content={"message": "User's data has been successfully changed"}, status_code=200)
